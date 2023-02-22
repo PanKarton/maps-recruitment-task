c26cf95e0b4d2bf01a71a54aad7ddae987b2ce61
unknown <jack-swad@wp.pl>
2023-02-22 20:42:36 +0100

    feat(routes-history): Added RoutesHistoryProvider

---
+++ providers/RoutesHistoryProvider.tsx
@@ -0,0 +1,32 @@
+import { createContext, useCallback, useContext, useState, ReactNode } from 'react';
+
+type Context = {
+  routesHistory: google.maps.DirectionsResult[];
+  addRouteToHistory: (route: google.maps.DirectionsResult) => void;
+};
+
+type Props = {
+  children: ReactNode;
+};
+
+export const RoutesHistoryContext = createContext<Context | null>(null);
+
+export const RoutesHistoryProvider = ({ children }: Props) => {
+  const [routesHistory, setRoutesHistory] = useState<google.maps.DirectionsResult[]>([]);
+
+  const addRouteToHistory = useCallback((route: google.maps.DirectionsResult) => {
+    setRoutesHistory(prevState => [...prevState, route]);
+  }, []);
+
+  const context = { routesHistory, addRouteToHistory };
+
+  return <RoutesHistoryContext.Provider value={context}>{children}</RoutesHistoryContext.Provider>;
+};
+
+export const useRoutesHistory = () => {
+  const data = useContext(RoutesHistoryContext);
+
+  if (!data) throw Error('useRoutePlanner must be used inside RoutePlannerProvider');
+
+  return data;
+};
