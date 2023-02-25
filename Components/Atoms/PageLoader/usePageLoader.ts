93b9b5db62bd1dba1e49cde611d8b0b43de30af2
unknown <jack-swad@wp.pl>
2023-02-25 12:47:41 +0100

    feat(loader): Added multipage loader based on router.events

--- Components/Atoms/PageLoader/usePageLoader.ts
+++ Components/Atoms/PageLoader/usePageLoader.ts
@@ -1,8 +1,13 @@
+import { useRouter } from 'next/router';
 import { useEffect, useState } from 'react';
 
 export const usePageLoader = () => {
+  const router = useRouter();
+
+  // const [loading, setLoading] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
 
+  // App loading
   useEffect(() => {
     const onPageLoad = () => {
       setIsLoading(false);
@@ -16,6 +21,26 @@ export const usePageLoader = () => {
       return () => window.removeEventListener('load', onPageLoad);
     }
   }, []);
+
+  // Page to page loading
+  useEffect(() => {
+    const handleStart = (url: string) => url !== router.asPath && setIsLoading(true);
+    const handleComplete = (url: string) =>
+      url === router.asPath &&
+      setTimeout(() => {
+        setIsLoading(false);
+      }, 2000);
+
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleComplete);
+    router.events.on('routeChangeError', handleComplete);
+
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleComplete);
+      router.events.off('routeChangeError', handleComplete);
+    };
+  }, [router.asPath, router.events]);
 
   return { isLoading };
 };
